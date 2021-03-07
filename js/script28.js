3495eca514d509a6c652731534d762a3787e9172
erfolg13 <erfolg_13@ukr.net>
2021-03-07 08:02:00 +0200

    added files index28 CSS script28

---
+++ js/script28.js
@@ -0,0 +1,33 @@
+//1. 1. Создать страницу, которая выводит нумерованный список песен:
+const playList = [
+    { author: "LED ZEPPELIN", song: "STAIRWAY TO HEAVEN" },
+    { author: "QUEEN", song: "BOHEMIAN RHAPSODY" },
+    { author: "LYNYRD SKYNYRD", song: "FREE BIRD" },
+    { author: "DEEP PURPLE", song: "SMOKE ON THE WATER" },
+    { author: "JIMI HENDRIX", song: "ALL ALONG THE WATCHTOWER" },
+    { author: "AC/DC", song: "BACK IN BLACK" },
+    { author: "QUEEN", song: "WE WILL ROCK YOU" },
+    { author: "METALLICA", song: "ENTER SANDMAN" },
+
+];
+const listItems = [];
+const listContainer = document.getElementById("listContainer");
+playList.forEach(element => listItems.push("<li>" + JSON.stringify(element) + "</li>"));
+
+listContainer.innerHTML = listItems.join('');
+
+//2. Создать HTML-страницу с кнопкой "Открыть" и модальным окном. На модальном окне должен быть текст и кнопка "Закрыть". Изначально модальное окно не отображается. При клике на кнопку "Открыть" появляется модальное окно, на кнопку "Закрыть" – исчезает.
+const popup = document.getElementById('popup');
+const openModal = document.querySelector('.popup-open');
+const closeModal = document.querySelector('.popup-close');
+
+openModal.onclick = function (e) {
+    e.preventDefault();
+    popup.style.visibility = 'visible';
+};
+
+closeModal.onclick = function () {
+    popup.style.visibility = 'hidden';
+};
+
+//3. Создать HTML-страницу со светофором и кнопкой, которая переключает светофор на следующий цвет.
