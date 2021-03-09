0b29df4ba794799623203c17d8b2e8c6281b007b
erfolg13 <erfolg_13@ukr.net>
2021-03-09 12:42:18 +0200

    added files index28, js28, style28 with the first exercise

--- js/script28.js
+++ js/script28.js
@@ -8,13 +8,26 @@ const playList = [
     { author: "AC/DC", song: "BACK IN BLACK" },
     { author: "QUEEN", song: "WE WILL ROCK YOU" },
     { author: "METALLICA", song: "ENTER SANDMAN" },
-
 ];
-const listItems = [];
-const listContainer = document.getElementById("listContainer");
-playList.forEach(element => listItems.push("<li>" + JSON.stringify(element) + "</li>"));
+// Перший спосіб
+// const listItems = [];
+// const listContainer = document.getElementById("listContainer");
+// playList.forEach(element => listItems.push("<li>" + JSON.stringify(element) + "</li>"));
 
-listContainer.innerHTML = listItems.join('');
+// listContainer.innerHTML = listItems.join('');
+
+// Другий спосіб
+const trackMusicList = document.getElementById('trackMusicList');
+const oderedList = document.createElement('ol');
+
+playList.forEach((list) => {
+    const listItem = document.createElement('li');
+    oderedList.appendChild(listItem);
+    const text = document.createTextNode('author: ' + list.author + ', ' + 'song: ' + list.song);
+    listItem.appendChild(text);
+});
+
+trackMusicList.appendChild(oderedList);
 
 //2. Создать HTML-страницу с кнопкой "Открыть" и модальным окном. На модальном окне должен быть текст и кнопка "Закрыть". Изначально модальное окно не отображается. При клике на кнопку "Открыть" появляется модальное окно, на кнопку "Закрыть" – исчезает.
 const popup = document.getElementById('popup');
@@ -31,3 +44,5 @@ closeModal.onclick = function () {
 };
 
 //3. Создать HTML-страницу со светофором и кнопкой, которая переключает светофор на следующий цвет.
+//Дороблю пізніше
+
