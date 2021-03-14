bda09f677c6b861659d0e13c09ab1472b5569a52
erfolg13 <erfolg_13@ukr.net>
2021-03-14 08:58:40 +0200

    added files in lesson31

---
+++ js/script31.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.images-containar').slick({
+        slidesToShow: 2,
+        dots: true,
+        speed: 300,
+        centerMode: true,
+        focusOnSelect: true,
+    });
+
+});
