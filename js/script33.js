11e41137410e1bf78916937425518cfa7e7bc44a
erfolg13 <erfolg_13@ukr.net>
2021-03-18 23:32:54 +0200

    modif js

--- js/script33.js
+++ js/script33.js
@@ -13,10 +13,6 @@ cathedralSophiaMarker.bindPopup(
 )
 var popup = L.popup();
 
-const imageUrl = 'http://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg',
-    imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
-L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
